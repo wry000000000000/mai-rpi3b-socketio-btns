c25a5a1e8e5a716af40ebc9958471e11ece9d8ec
wry000000000000 <wry_by01@outlook.com>
2024-08-06 15:06:52 +0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,13 +1,16 @@
+'''Client File(install on your RaspberryPi)
+客户端文件（装在你的树莓派上）
+'''
 import time
 import gpiozero
 import socketio
 gps=[]
-gps+=[2,3,4,17,27,22,10,9]
+gps+=[2,3,4,17,27,22,10,9]# TODO:modify it to your real GPIO numbers(not pinout number)(optional,you can use the default) 将其修改为你实际连接的GPIO号（可选，你可以按照默认值安装）
 btns=[]
 for i in range(8):
     btns+=[gpiozero.Button(gps[i])]
 s0=socketio.client.Client()
-s0.connect("http://192.168.31.102:8000")
+s0.connect("http://192.168.31.102:8000")# TODO:Modify it to your mainunit's IP 将其修改为你的主机IP
 isos={}
 for i in btns:
     isos[i]=False
